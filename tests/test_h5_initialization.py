3f7212398ccd81b443dddb9da7fa43846f09e37e
Tharayil Joseph <joseph.tharayil@epfl.ch>
2023-11-29 17:43:19 +0100

    fixes iloc warning

--- tests/test_h5_initialization.py
+++ tests/test_h5_initialization.py
@@ -3,18 +3,27 @@ import pandas as pd
 import bluepy as bp
 import numpy as np
 
-from writeH5_prelim import count_segments
+from writeH5_prelim import makeElectrodeDict, count_segments
 
 @pytest.fixture
 def secCounts():
 
-'''
-Defines a data frame containing gids and section ids, equivalent to teh column indices from a voltage report
-'''
+    '''
+    Defines a data frame containing gids and section ids, equivalent to teh column indices from a voltage report
+    '''
 
     sec_counts = pd.DataFrame(data=np.array([[1,1,1,1,1,1,2,2,2,2,2,2],[0,1,1,1,1,1,0,1,1,1,1,1]]).T,columns=('gid',bp.Section.ID))
     
     return sec_counts
+
+def test_makeElectrodeDict():
+
+    csv = '/gpfs/bbp.cscs.ch/project/proj83/tharayil/generationCode/create_lfp_weights_for_neurodamus/tests/data/electrode.csv'
+    expected = {'name':{'position':np.array([1,2,3]),'type':'EEG','region':'Outside','layer':'Outside'}}
+
+    print(makeElectrodeDict(csv,'EEG'))
+    
+    assert makeElectrodeDict(csv,'EEG') == expected
 
 def test_count_segments(secCounts):
     

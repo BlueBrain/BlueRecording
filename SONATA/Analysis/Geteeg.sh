73f7c90f631cd23e6d05664b66b225df2798a3c3
Tharayil Joseph <joseph.tharayil@epfl.ch>
2023-12-23 15:49:22 +0100

    Adds analysis script

---
+++ SONATA/Analysis/Geteeg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH --job-name="EEG_2_CoordsV"
+#SBATCH --partition=prod
+#SBATCH --nodes=14
+#SBATCH -C clx
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2:00:00
+##SBATCH --mail-type=ALL
+#SBATCH --account=proj45
+#SBATCH --no-requeue
+#SBATCH --output=EEG_0_CoordsV.out
+#SBATCH --error=EEG_0_CoordsV.err
+#SBATCH --exclusive
+#SBATCH --mem=0
+
+
+#spack load py-bluepy/jrzr2b
+#module load unstable py-bluepy py-mpi4py
+
+module load unstable py-bluepysnap py-bluepy
+
+mkdir pkls
+
+srun -n 420 python geteeg.py
+
---
+++ SONATA/Analysis/geteeg.py
@@ -0,0 +1,38 @@
+import bluepysnap as bp
+from bluepysnap.sonata_constants import Node
+import numpy as np
+import pandas as pd
+import sys
+from mpi4py import MPI
+import os 
+
+rank = MPI.COMM_WORLD.Get_rank()
+
+nranks = MPI.COMM_WORLD.Get_size()
+
+
+s = bp.Simulation('simulation_config.json')
+population_name = s.reports['lfp_report'].population_names[0]
+
+r  = s.reports['lfp_report'][population_name]
+
+types = list(s.circuit.nodes.property_values('mtype'))
+regions = ['S1FL','S1DZ','S1DZO','S1HL','S1Sh','S1Tr','S1ULp']
+
+tIdx = int(rank/len(regions))
+rIdx = rank % len(regions)
+
+nodes = s.circuit.nodes
+regionIds = nodes.ids(group=regions[rIdx])
+typeIds = nodes.ids(group=types[tIdx])
+
+ids = regionIds.intersection(typeIds).get_ids()
+
+eeg = r.get(group=ids,t_start=2000,t_step=1)
+
+
+eeg.columns = pd.MultiIndex.from_product((np.unique(eeg.columns.get_level_values(0)),['Forelimb','Shoulder','Testing']),names=['gid','electrode'])
+eeg = eeg.sum(axis=1,level='electrode')
+
+
+eeg.to_pickle('pkls/eeg_'+regions[rIdx]+'_'+types[tIdx]+'.pkl')

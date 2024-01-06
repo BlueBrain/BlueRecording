Fixes bug in dipole calculation

SONATA/tests/conftest.py
@@ -227,6 +227,11 @@ def write_EField(path_to_potentialfield_file):
     xaxis = np.linspace(-10,10)*1e-6
     yaxis = np.linspace(-10,10)*1e-6
     zaxis = np.linspace(-10,10)*1e-6
+
+    xcenter = (xaxis[1:]+xaxis[:-1])/2
+    ycenter = (yaxis[1:]+yaxis[:-1])/2
+    zcenter = (zaxis[1:]+zaxis[:-1])/2
+
     realImag = np.array([0,1])
 
     meshes = file.create_group('Meshes')
@@ -243,9 +248,12 @@ def write_EField(path_to_potentialfield_file):
     snapshot = obj.create_group('Snapshots')
     field0 = snapshot.create_group('0')
     
-    xd, yd, zd,rd = np.meshgrid(xaxis[1:],yaxis[1:],zaxis[1:],realImag,indexing='ij')
-    comp0 = field0.create_dataset('comp0',data =zd)
-    comp1 = field0.create_dataset('comp1',data =zd)
+    xd, __, __, __ = np.meshgrid(xcenter,yaxis,zaxis,realImag,indexing='ij')
+    __, yd, __, __ = np.meshgrid(xaxis,ycenter,zaxis,realImag,indexing='ij')
+    __, __, zd, __ = np.meshgrid(xaxis,yaxis,zcenter,realImag,indexing='ij')
+    
+    comp0 = field0.create_dataset('comp0',data =xd)
+    comp1 = field0.create_dataset('comp1',data =yd)
     comp2 = field0.create_dataset('comp2',data =zd)
     
 

SONATA/tests/test_writeH5_SONATA.py
@@ -181,7 +181,7 @@ def test_get_coeffs_dipoleReciprocity(positions,write_EField,gids):
     
     testPositions = getSegmentMidpts(positions,gids)
     
-    center = np.mean(testPositions,axis=1)
+    center = testPositions.mean(axis=1)
     
     potentials = get_coeffs_dipoleReciprocity(testPositions,write_EField,center)
         
@@ -191,7 +191,7 @@ def test_get_coeffs_dipoleReciprocity(positions,write_EField,gids):
     
     columnMultiIndex = pd.MultiIndex.from_tuples(columnIdx,names=['id','section'])
     
-    expectedPotential = pd.DataFrame(data=np.array([0,0.5e-6])[np.newaxis,:]**2,columns=columnMultiIndex)
+    expectedPotential = pd.DataFrame(data=np.array([0.5e-6,0])[np.newaxis,:]**2,columns=columnMultiIndex)
         
     pd.testing.assert_frame_equal(potentials,expectedPotential)
     

SONATA/writeH5.py
@@ -152,9 +152,12 @@ def get_coeffs_dipoleReciprocity(positions, path_to_fields,center):
 
     '''
     path_to_fields is the path to the h5 file containing the potential field, outputted from Sim4Life
-    path_to_positions is the path to the output from the position-finding script
     '''
 
+    center = center.values
+    positionColumns = positions.columns
+    positions = positions.values
+    
     # Get new output file potential field
 
     with h5py.File(path_to_fields, 'r') as f:
@@ -188,6 +191,8 @@ def get_coeffs_dipoleReciprocity(positions, path_to_fields,center):
 
     center *= 1e-6
 
+    positions -= center[:,np.newaxis]
+
 
     InterpFcnX = RegularGridInterpolator((xCenter, y, z), Ex[:, :, :, 0], method='linear')
     InterpFcnY = RegularGridInterpolator((x, yCenter, z), Ey[:, :, :, 0], method='linear')
@@ -199,10 +204,10 @@ def get_coeffs_dipoleReciprocity(positions, path_to_fields,center):
     
     ZComp = InterpFcnZ(center)[np.newaxis]  # Interpolate E field at location of neural center
     
-    out2rat = positions.values[0]*XComp + positions.values[1]*YComp + positions.values[2]*ZComp
+    out2rat = positions[0]*XComp + positions[1]*YComp + positions[2]*ZComp
 
 
-    outdf = pd.DataFrame(data=(out2rat / currentApplied), columns=positions.columns) # Scale potential field by applied current
+    outdf = pd.DataFrame(data=(out2rat / currentApplied), columns=positionColumns) # Scale potential field by applied current
 
     return outdf
 
@@ -470,7 +475,7 @@ def writeH5File(path_to_simconfig,segment_position_folder,outputfile,numFilesPer
             
                 if electrodeType == 'DipoleReciprocity':
 
-                    center = np.mean(newPositions,axis=1)
+                    center = newPositions.mean(axis=1)
 
                     coeffs = get_coeffs_dipoleReciprocity(newPositions,path_to_fields[reciprocityIdx],center)
 
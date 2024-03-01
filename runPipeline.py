a05ac910bf8bed7824de84d5739efc8ae2a25c9a
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-03-01 16:15:08 +0100

    Reorganization

--- runPipeline.py
+++ runPipeline.py
@@ -1,4 +1,12 @@
-from SONATA.getPositions import getPositions
+from bluerecording import *
+
+
+makePositionFolders(path_to_simconfig,chunk_size,path_to_positions_folder)
+
+getPositions(path_to_simconfig, chunk_size, path_to_positions_folder,replace_axons=True)
+
+writeH5File(path_to_simconfig,outputfile,electrode_csv)
+
 
 
 if __name__=='__main__':

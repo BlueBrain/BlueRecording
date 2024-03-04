f6092b2464e23df5ef8cedcaad3a28481a87cbfb
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-03-04 13:42:59 +0100

    Reorganizes examples

--- run_get_positions.py
+++ run_get_positions.py
@@ -3,15 +3,17 @@ from bluerecording.getPositions import getPositions
 
 if __name__=='__main__':
 
-    path_to_simconfig = sys.argv[1] #simulation_condif with one-timestep simulation outputting a compartment report
+    path_to_simconfig = sys.argv[1] #simulation_config with one-timestep simulation outputting a compartment report
 
     path_to_positions_folder = sys.argv[2]
-
-    chunk_size = int(sys.argv[3]) # Number of pickle files to write to each subfolder
     
-    if len(sys.argv)>4:
-        replace_axons = sys.argv[4]
+    neurons_per_file = int(sys.argv[3])
+
+    chunk_size = int(sys.argv[4]) # Number of pickle files to write to each subfolder
+    
+    if len(sys.argv)>5:
+        replace_axons = sys.argv[5]
     else:
         replace_axons = True
 
-    getPositions(path_to_simconfig, chunk_size, path_to_positions_folder,replace_axons)
+    getPositions(path_to_simconfig, neurons_per_file, files_per_folder, path_to_positions_folder,replace_axons)

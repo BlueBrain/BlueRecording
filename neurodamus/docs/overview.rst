e366954e139083b48b665b1cf0bc94c04220ed12
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-04-18 14:39:11 +0200

    Removes dependencies

--- neurodamus/docs/overview.rst
+++
@@ -1,9 +0,0 @@
-.. include:: ../README.rst
-    :end-before: .. substitutions
-
-License
-=======
-
-.. literalinclude:: ../LICENSE.txt
-
-.. |banner| image:: ./img/neurodamus_banner_230701.png
--- neurodamus/tests/scientific/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import pytest
-
-assert os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"), \
-    "Test requires loading a neocortex model to run"
-
-pytestmark = [
-    pytest.mark.forked,
-]

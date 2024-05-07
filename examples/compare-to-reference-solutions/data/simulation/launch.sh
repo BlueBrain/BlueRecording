af21089349bfdea53c49d4d25c118c9ce39da6ff
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-05-07 21:57:33 +0200

    Adds module.yaml

--- examples/compare-to-reference-solutions/data/simulation/launch.sh
+++ examples/compare-to-reference-solutions/data/simulation/launch.sh
@@ -10,11 +10,9 @@
 #SBATCH --job-name=CortexNrdmsPySim
 
 
-export NEURODAMUS_PYTHON=/gpfs/bbp.cscs.ch/home/tharayil/spack_install/software/install_gcc-12.3.0-skylake/py-neurodamus-3.2.conductance-qaaluj/lib/python3.11/site-packages/neurodamus/data
-#export  MOD_FILE_PATH=/gpfs/bbp.cscs.ch/home/tharayil/spack_install/software/install_oneapi-2023.2.0-skylake/neurodamus-models-42.42.42_conductance-hl4mep/share/neurodamus_neocortex/
+spack env activate bluerecording
+module load unstable
+module load neurodamus-neocortex/42.42.42_conductance neuron/9.0.a16_conductance 
+module load py-neurodamus/3.2.conductance 
 
-#cp -r $MOD_FILE_PATH/mod .
-
-#nrnivmodl mod
-
-srun dplace ./x86_64/special -mpi -python $NEURODAMUS_PYTHON/init.py --configFile=simulation_config.json --lb-mode=RoundRobin
+srun dplace special -mpi -python $NEURODAMUS_PYTHON/init.py --configFile=simulation_config.json --lb-mode=RoundRobin

590defd8a284b6147057e41ae6cf4ca267809988
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-05-13 12:02:32 +0200

    Reverts module names to 'develop'

--- examples/SSCx/data/simulation/launch.sh
+++ examples/SSCx/data/simulation/launch.sh
@@ -11,6 +11,6 @@
 
 spack env activate neurodamus
 module load unstable
-module load neurodamus-neocortex/unstable neuron/develop py-neurodamus/unstable
+module load neurodamus-neocortex/develop neuron/develop py-neurodamus/develop
 
 srun dplace special -mpi -python $NEURODAMUS_PYTHON/init.py --configFile=simulation_config.json --lb-mode=RoundRobin

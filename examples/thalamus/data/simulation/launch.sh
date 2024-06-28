abcb29bb82b9f71b313b313ef82d7a9654cf98e4
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-06-28 17:31:48 +0200

    Adds thalamus

---
+++ examples/thalamus/data/simulation/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --account=proj55
+#SBATCH --partition=prod
+#SBATCH --nodes=32
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=0
+#SBATCH --constraint=cpu
+#SBATCH --exclusive
+#SBATCH --time=24:00:00
+#SBATCH --job-name=CortexNrdmsPySim
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+module load unstable
+module load neurodamus-thalamus py-neurodamus
+
+srun dplace special -mpi -python $NEURODAMUS_PYTHON/init.py --configFile=simulation_config.json --lb-mode=RoundRobin
